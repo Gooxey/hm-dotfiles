d59ecf8e3464b4d12ffd2d773a9f7516d36935f9
Gooxey <dennis.tiderko@gmail.com>
2024-10-09 20:40:01 +0200

    doom config will now be linked impurely

--- emacs/default.nix
+++ emacs/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   programs.emacs.enable = true;
   services.emacs = {
@@ -9,7 +9,11 @@
     };
   };
 
-  xdg.configFile.doom.source = ./config/doom;
+  home.activation.linkMyFiles = config.lib.dag.entryAfter ["writeBoundry"] ''
+    if [ ! -e $HOME/.config/doom ]; then
+      ln -s $HOME/hm-dotfiles/emacs/config/doom $HOME/.config
+    fi
+  '';
 
   programs.java.enable = true;
   home.packages = with pkgs; [
